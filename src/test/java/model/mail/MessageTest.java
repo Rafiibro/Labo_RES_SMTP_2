e4c1b55ca352751269c170a7a05bab3873179d82
rafiibro <rafael.dacunhagarcia@icloud.com>
2019-04-11 09:21:36 +0200

    upgraade

--- src/test/java/model/mail/MessageTest.java
+++ src/test/java/model/mail/MessageTest.java
@@ -6,6 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MessageTest {
 
+    @Test
+    void createMessage() {
 
+    }
 
 }
--- src/test/java/model/mail/PersonneTest.java
+++ src/test/java/model/mail/PersonneTest.java
@@ -7,12 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PersonneTest {
 
     @Test
-    void createPersonnne() {
+    public void createOnePersonnne() {
         Personne p1 = new Personne("Rafael", "Garcia", "grcia.raf@gmail.com");
 
         assertEquals("grcia.raf@gmail.com", p1.getAdresse());
         assertEquals("Rafael", p1.getPrenom());
         assertEquals("Garcia", p1.getNom());
-
     }
 }
